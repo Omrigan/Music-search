1e34ba5140bb3a6ad7e8f9d1af0c663b01b62b78
Omrigan <gogmig@gmail.com>
2013-10-27 00:14:22 +0800

    Initial commit

---
+++ src/net/music_search/MyActivity.java
@@ -0,0 +1,15 @@
+package net.music_search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
